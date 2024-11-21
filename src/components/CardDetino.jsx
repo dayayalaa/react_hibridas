2a72a47ab17bb8da0fb1a64d9e8134588a8f4907
Mailen <maimonney30@gmail.com>
2024-11-21 19:58:20 -0300

    se hizo un resumen de descripcion

--- src/components/CardDetino.jsx
+++ src/components/CardDetino.jsx
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom";
 import '../app.css';
 
 const TarjetaDestino = ({ destino }) => {
+  const descripcionLimitada = destino.descripcion.length > 100 ? destino.descripcion.slice(0, 100) + "..." : destino.descripcion;
+
   return (
     <div className="card">
       <img className="card-image" src={destino.imagen} alt={destino.nombre} />
       <div className="card-content">
         <h3 className="card-title">{destino.nombre}</h3>
-        <p className="card-description">{destino.descripcion}</p>
+        <p className="card-description">{descripcionLimitada}</p>
         <Link to={`/detalles/${destino._id}`} className="card-button">
-                Ver más
-            </Link>
+          Ver más
+        </Link>
       </div>
     </div>
   );
